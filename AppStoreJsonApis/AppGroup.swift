d2ecad2eeecf38da5d439ed4ff61789ea7747665
jack sp@rrow <stepanet@gmail.com>
2019-05-04 01:13:55 +0100

    commit

---
+++ AppStoreJsonApis/AppGroup.swift
@@ -0,0 +1,22 @@
+//
+//  AppGroup.swift
+//  AppStoreJsonApis
+//
+//  Created by Jack Sp@rroW on 03/05/2019.
+//  Copyright © 2019 Jack Sp@rroW. All rights reserved.
+//
+
+import Foundation
+
+struct AppGroup: Decodable {
+    let feed: Feed
+}
+
+struct Feed: Decodable {
+    let title: String
+    let results: [FeedResult]
+}
+
+struct FeedResult: Decodable {
+    let name, artistName, artworkUrl100: String
+}
